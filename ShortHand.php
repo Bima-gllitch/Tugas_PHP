9796f222310d2c300dbe491cf3dc0461554e3e0d
Bima-gllitch <freyandro@gmail.com>
2025-04-29 14:21:19 +0700

    bima

--- ShortHand.php
+++ ShortHand.php
@@ -3,6 +3,6 @@ $iphone = 10;
 $jumlah_sewa = 7;
 
 echo ($jumlah_sewa <= $iphone) 
-    ? "Kursi tersedia, $jumlah_sewa kursi berhasil dipinjam." 
-    : "Stok tidak mencukupi, hanya tersedia $iphone kursi.";
+    ? "iphone tersedia, $jumlah_sewa iphone berhasil disewa." 
+    : "Stok tidak mencukupi, hanya tersedia $iphone iphone.";
 ?>
